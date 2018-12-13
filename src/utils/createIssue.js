bd7560fa978133de3b2fec8b1459d2954f6a2456
jdmedlock <jdmedlock@gmail.com>
2018-12-13 07:52:29 -0600

    Refactor: Add GitHub access token to `.env` file
    Add the GitHub access token used in `createIssue.js` to the `.env` file to protect it
    - Add `.env` file to `.gitignore`
    - Add conditional to produce additional information about errors and GitHub limits to console log only when in development mode.
    
    Resolves: N/a
    See also: N/a

--- src/utils/createIssue.js
+++ src/utils/createIssue.js
@@ -2,14 +2,14 @@ import createGHIssue from 'github-create-issue';
 
 const createIssue = (title, body) => {
   var opts = {
-    'token': 'e7177ac5b68cc1bd35afdc8b5b71942fa8e652ef',
+    'token': process.env.REACT_APP_GITHUB_ACCESS_TOKEN,
     'body': body,
     'labels': ['bug'],
   };
 
   createGHIssue( 'jdmedlock/meteorite', title, opts, ( error, issue, info ) => {
     // Check for rate limit information...
-    if ( info ) {
+    if ( info && process.env.NODE_ENV === 'development') {
         console.error( 'Limit: %d', info.limit );
         console.error( 'Remaining: %d', info.remaining );
         console.error( 'Reset: %s', (new Date( info.reset*1000 )).toISOString() );
@@ -17,7 +17,9 @@ const createIssue = (title, body) => {
     if ( error ) {
         throw new Error( error.message );
     }
-    console.log( JSON.stringify( issue ) );
+    if (process.env.NODE_ENV === 'development') {
+      console.log( JSON.stringify(issue));
+    }
   });
 
 };
