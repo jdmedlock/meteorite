208451da9fdc6ab4026b776d0d3e300dc7330fbd
jdmedlock <jdmedlock@gmail.com>
2018-12-13 10:41:05 -0600

    Feature: Add URL to GitHub Issue in error message
    Add URL to GitHub Issue in error message
    - Refactor createIssue.js to be synchronous
    
    Resolves: N/a
    See also: N/a

--- src/components/SearchErrorBoundary.js
+++ src/components/SearchErrorBoundary.js
@@ -4,22 +4,43 @@ import { createIssue } from '../utils/createIssue';
 class SearchErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { 
+      hasError: false,
+      issueTicketJSON: null,
+    };
   }
 
   static getDerivedStateFromError(error) {
     return { hasError: true };
   }
 
-  componentDidCatch(error, info) {
-    console.log(createIssue('Search error', 'Test bug report generation'));
+  async componentDidCatch(error, info) {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(info);
+    }
+    try {
+      const issueResults = await createIssue('Search error', 'Test bug report generation');
+      this.setState({ issueTicketJSON: issueResults });
+    }
+    catch(error) {
+      console.log('error: ', error);
+      throw new Error('Unable to create issue ticket');
+    }
   }
 
   render() {
     if (this.state.hasError) {
       return (
         <div>
-          <h1>You searched for a vulgar word! "{this.props.searchTerms}" is prohibited</h1>
+          <h3>You searched for a vulgar word! "{ this.props.searchTerms }" is prohibited.</h3>
+          <h5>
+            An issue has been automatically created for this error on 
+            <a 
+              href={ this.state.issueTicketJSON ? this.state.issueTicketJSON.html_url : ' ' }
+              target="_blank" rel="noopener noreferrer">
+              &nbsp;GitHub
+            </a>
+          </h5>
         </div>
       );
     }
--- src/utils/createIssue.js
+++ src/utils/createIssue.js
@@ -1,27 +1,32 @@
 import createGHIssue from 'github-create-issue';
 
-const createIssue = (title, body) => {
-  var opts = {
+const createIssue = async (title, body) => {
+  const opts = {
     'token': process.env.REACT_APP_GITHUB_ACCESS_TOKEN,
     'body': body,
     'labels': ['bug'],
   };
 
-  createGHIssue( 'jdmedlock/meteorite', title, opts, ( error, issue, info ) => {
-    // Check for rate limit information...
-    if ( info && process.env.NODE_ENV === 'development') {
-        console.error( 'Limit: %d', info.limit );
-        console.error( 'Remaining: %d', info.remaining );
-        console.error( 'Reset: %s', (new Date( info.reset*1000 )).toISOString() );
-    }
-    if ( error ) {
-        throw new Error( error.message );
-    }
-    if (process.env.NODE_ENV === 'development') {
-      console.log( JSON.stringify(issue));
-    }
+  let issueIsCreated = new Promise((resolve, reject) => {
+    createGHIssue( 'jdmedlock/meteorite', title, opts, ( error, issue, info ) => {
+      // Display rate limit information...
+      if ( info && process.env.NODE_ENV === 'development') {
+          console.error( 'Limit: %d', info.limit );
+          console.error( 'Remaining: %d', info.remaining );
+          console.error( 'Reset: %s', (new Date( info.reset*1000 )).toISOString() );
+      }
+      if ( error ) {
+          throw new Error( error.message );
+      }
+      if (process.env.NODE_ENV === 'development') {
+        console.log( JSON.stringify(issue));
+      }
+      resolve(issue);
+    });
   });
 
+  let issue = await issueIsCreated;
+  return issue;
 };
 
 export { createIssue };
